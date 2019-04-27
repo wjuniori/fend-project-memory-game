3610ae0abafe25c56dac66e590e713569f6052d7
wjuniori <wjuniori.si@gmail.com>
2019-04-27 15:49:50 -0300

    feat: Build your own version of the classic memory game 'Concentration'
    The game board consists of sixteen "cards" arranged in a grid. The deck
    is made up of eight different pairs of cards, each with different symbols
    on one side. The cards are arranged randomly on the grid with the symbol
    face down. The gameplay rules are very simple: flip over two hidden cards
    at a time to locate the ones that match!
    
    Each turn:
    
    - The player flips one card over to reveal its underlying symbol.
    - The player then turns over a second card, trying to find the corresponding
      card with the same symbol.
    - If the cards match, both cards stay flipped over.
    - If the cards do not match, both cards are flipped face down.
    
    The game ends once all cards have been correctly matched.

--- README.md
+++ README.md
@@ -1,20 +1,39 @@
-# Memory Game Project
+# fend-project-memory-game
 
-## Table of Contents
+This repository contains the resources and source code of the memory game. The fend-project-memory-game was developed during the [Udacity] Nanodegree Fundamentos Web Front-End.
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+## How the Game Works
 
-## Instructions
+The game board consists of sixteen "cards" arranged in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. The cards are arranged randomly on the grid with the symbol face down. The gameplay rules are very simple: flip over two hidden cards at a time to locate the ones that match!
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+Each turn:
 
-To get started, open `js/app.js` and start building out the app's functionality
+* The player flips one card over to reveal its underlying symbol.
+* The player then turns over a second card, trying to find the corresponding card with the same symbol.
+* If the cards match, both cards stay flipped over.
+* If the cards do not match, both cards are flipped face down.
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+The game ends once all cards have been correctly matched.
 
-## Contributing
+## Preview
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+* **[Live Preview (GitHub Pages)](https://wjuniori.github.io/fend-project-memory-game/)**
 
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+## Download
+
+* Clone the repo: `git clone https://github.com/wjuniori/fend-project-memory-game.git`
+* [Fork, Clone, or Download on GitHub](https://github.com/wjuniori/fend-project-memory-game)
+
+## Usage
+
+### Basic Usage
+
+After downloading, simply edit the HTML, CSS and JS files included with the website in your favorite text editor to make changes. These are the only files you need to worry about, you can ignore everything else! To preview the changes you make to the code, you can open the `index.html` file in your web browser.
+
+## Bugs and Issues
+
+Have a bug or an issue with this website? [Open a new issue](https://github.com/wjuniori/fend-project-memory-game/issues) here on GitHub.
+
+## About
+
+This repository was created by and is maintained by **[Washington Júnior](https://github.com/wjuniori/)**. To see more projects, visit [portfolio](https://wjuniori.github.io/).
--- js/app.js
+++ js/app.js
@@ -1,38 +1,218 @@
-/*
- * Create a list that holds all of your cards
- */
+'use strict'; // Start of use strict
 
+// Temporarily stores opened cards
+let listOpenCards = [];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// Stores call of setTimeout function
+let timeID;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
+// Reset the entire grid
+function resetGrid() {
+
+  const deck = $('.deck');
+  deck.css('display', 'none');
+  const cards = deck.children();
+  cards.remove();
+  cards.removeClass('open show match');
+  shuffle(cards);
+  deck.append(cards);
+  deck.removeAttr('style');
+
+}
+
+// Timer
+function countTime() {
+
+  let timer = $('.timer').text().split(':');
+
+  for (var i = timer.length - 1; i >= 0; i--) {
+
+    // Turn of minutes and seconds after 59
+    if (i !== 0 && Number(timer[i]) + 1 === 60) {
+      timer[i] = '00';
+    } else {
+      timer[i] = Number(timer[i]) + 1 < 10 ? '0' + (Number(timer[i]) + 1) : Number(timer[i]) + 1;
+      break;
+    }
+
+  }
+
+  $('.timer').text(timer.join(':'));
+
+  timeID = setTimeout(countTime, 1000);
+
+}
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+* Create a list that holds all of your cards
+* Display the cards on the page
+*   - shuffle the list of cards using the provided "shuffle" method above
+*   - loop through each card and create its HTML
+*   - add each card's HTML to the page
+*
+* Reset the move counter, timer and star rating
+*/
+function initializeGame() {
+
+  clearTimeout(timeID);
+  listOpenCards = [];
+  resetGrid();
+  $('.moves').text(0);
+  $('.stars').find('i').removeClass('display-none');
+  $('.timer').text('00:00:00');
+  timeID = setTimeout(countTime, 1000);
+
+}
+
+// Display the card's symbol
+function displayCardSymbol(card) {
+
+  card.addClass('open show');
+
+}
+
+// Add the card to a *list* of "open" cards
+function addOpenCardList(card) {
+
+  listOpenCards.push(card);
+
+}
+
+/*
+* If the list already has another card, check to see if the two cards match
+*   - if the cards do match, lock the cards in the open position
+*   - if the cards do not match, remove the cards from the list and hide the card's symbol
+*/
+function checkMatchedCards() {
+
+  if (listOpenCards.length === 2) {
+
+    if (listOpenCards[0].children().attr('class') === listOpenCards[1].children().attr('class')) {
+
+      listOpenCards.forEach(function(card) {
+        card.toggleClass('open show match');
+      });
+
+    } else {
+
+      listOpenCards.forEach(function(card) {
+        card.removeClass('open show');
+      });
+
+    }
+
+    listOpenCards = [];
+
+  }
+
+}
+
+// Increment the move counter and display it on the page
+function incrementMoveCounter() {
+
+  const count = Number($('.moves').text()) + 1;
+
+  $('.moves').text(count);
+
+  switch(count) {
+    case 13:
+    case 25:
+      const stars = $('.stars').find('i').not('.display-none');
+      stars.first().addClass('display-none');
+      break;
+  }
+
+}
+
+// Check if all cards are matched
+function checkGameEnd() {
+
+  const cards = $('.card');
+
+  for (const card of cards) {
+
+    if (!$(card).hasClass('match')) {
+      return false;
+    }
+
+  }
+
+  return true;
+
+}
+
+// Display a message with the final score
+function displayCongratulations() {
+
+  clearTimeout(timeID);
+
+  $(`<p>With ${$('.moves').text()} Moves and ${$('.stars').find('i').not('.display-none').length} Stars in ${$('.timer').text()}</p>`).insertAfter('.modal-content h2');
+
+  $('.modal').attr('style', 'display: block');
+
+}
+
+// Executes when DOM is fully loaded
+$(function() {
+
+  initializeGame();
+
+  // Executes when card (a li element) is clicked
+  // Delegates the click event to card (li element)
+  $('.deck').on('click', '.card', function(event) {
+
+    const card = $(event.target).is('li') ? $(event.target) : $(event.target).parent();
+
+    if (!card.hasClass('open') && !card.hasClass('match') && listOpenCards.length < 2) {
+
+      displayCardSymbol(card);
+      addOpenCardList(card);
+
+      if (listOpenCards.length === 2) {
+
+        setTimeout(function() {
+
+          checkMatchedCards();
+          incrementMoveCounter();
+          if (checkGameEnd()) {
+            displayCongratulations();
+          }
+
+        }, 800);
+
+      }
+
+    }
+
+  });
+
+  // Resets the entire grid as well as the move counter, timer and star rating when div.restart is clicked
+  $('.restart').on('click', function() {
+    initializeGame();
+  });
+
+  // Closes the modal and resets the entire grid as well as the move counter, timer and star rating when button.btn-modal is clicked
+  $('.btn-modal').click(function() {
+    $('.modal').attr('style', null);
+    $('.modal-content p').first().remove();
+    initializeGame();
+  });
+
+});
+
+// End of use strict
